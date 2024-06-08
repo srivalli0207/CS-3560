2acda8bc7a706b99d6410492496a58d56d7c7bbf
Srivalli Kakumani <144284239+srivalli0207@users.noreply.github.com>
2024-06-07 20:50:26 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -4,22 +4,22 @@ import java.util.*;
 public class Question 
 {
 	private QuestionType type;
-    private List<String> candidateAnswers;
+        private List<String> candidateAnswers;
 
-    // Constructor to initialize a question with type and candidate answers
-    public Question(QuestionType type, List<String> candidateAnswers) 
-    {
-    	this.type = type;
-        this.candidateAnswers = candidateAnswers;
-    }
+	// Constructor to initialize a question with type and candidate answers
+	public Question(QuestionType type, List<String> candidateAnswers) 
+	{
+		this.type = type;
+		this.candidateAnswers = candidateAnswers;
+	}
 
-    public QuestionType getType() 
-    {
-    	return type;
-    }
+	public QuestionType getType() 
+	{
+		return type;
+	}
 
-    public List<String> getCandidateAnswers() 
-    {
-    	return candidateAnswers;
-    }
+	public List<String> getCandidateAnswers() 
+	{
+		return candidateAnswers;
+	}
 }
